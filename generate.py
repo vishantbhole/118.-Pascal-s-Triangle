89b41a0d2d42e28dbaeb629a95c43ec8c6b7cbb9
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-07-08 21:29:28 -0500

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,2 +1,23 @@
 # 118. Pascal's Triangle
 
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = []
+
+        for i in range(numRows):
+            row = [1]
+            if i > 0:
+                prev_row = result[i - 1]
+                for j in range(1, i):
+                    row.append(prev_row[j - 1] + prev_row[j])
+                row.append(1)
+
+            result.append(row)
+
+        return result
+
+
